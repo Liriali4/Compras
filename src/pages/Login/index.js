17b48e63838de26e842e0a5d5e62e6be07fa9038
Líria Li4 <lirialia51@gmail.com>
2023-02-17 15:49:37 +0100

    criado o contexto usuário

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,29 +2,32 @@ import { Button } from '@material-ui/core';
 import { Container, Titulo, InputContainer } from './styles';
 import { Input, InputLabel, InputAdornment } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import { UsuarioContext } from 'common/context/usuario';
+import { useContext } from 'react'
 
-function Login({ nome, setNome, saldo, setSaldo }) {
+function Login() {
   const history = useHistory();
-    return (
-      <Container>
-        <Titulo>
-          Insira o seu nome
-        </Titulo>
-        <InputContainer>
-          <InputLabel>
-            Nome
-          </InputLabel>
-          <Input
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
+  return (
+    <Container>
+      <Titulo>
+        Insira o seu nome
+      </Titulo>
+      <InputContainer>
+        <InputLabel>
+          Nome
+        </InputLabel>
+        <Input
           value={nome}
-          onChange={(event)=> setNome(event.target.value)}
+          onChange={(event) => setNome(event.target.value)}
           type="text"
-          />
-        </InputContainer>
-        <InputContainer>
-          <InputLabel>
-            Saldo
-          </InputLabel>
-          <Input
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputLabel>
+          Saldo
+        </InputLabel>
+        <Input
           value={saldo}
           onChange={(event) => setSaldo(event.target.value)}
           type="number"
@@ -34,16 +37,16 @@ function Login({ nome, setNome, saldo, setSaldo }) {
             </InputAdornment>
           }
         />
-        </InputContainer>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => history.push('/feira') }
-        >
-          Avançar
-        </Button>
-      </Container>
-    )
+      </InputContainer>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => history.push('/feira')}
+      >
+        Avançar
+      </Button>
+    </Container>
+  )
 };
 
 export default Login;
